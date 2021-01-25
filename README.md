357fde7f8f450e4fd4242228ff448dbd8d30e638
RuanAlesi <alesi.ruan@gmail.com>
2021-01-24 22:39:12 -0300

    ajustando

---
+++ README.md
@@ -0,0 +1,15 @@
+# Selenium practicing
+I have in this repository the code wrote as a hiring process test.
+
+## Preconditions
+It's necessary these follow systems:
+- Maven 3.6.3
+- JDK 1.8
+- Google Chrome browser
+
+## Installing
+- Clone this project in your directory
+- In the directory, run 'mvn clean install'. It installs the project dependencies
+
+## Running tests
+- Run 'mvn test' command line to run all tests
--- src/main/java/Objetos/Sessao.java
+++ src/main/java/Objetos/Sessao.java
@@ -2,12 +2,15 @@ package Objetos;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Sessao {
 	private WebDriver driver;
 	
 	public Sessao() {
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		driver = new ChromeDriver(options);
 	}
 	
 	public WebDriver getDriver (){
